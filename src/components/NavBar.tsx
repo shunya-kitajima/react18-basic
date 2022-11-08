2b1a375fb04a2b0446b9fb9b9ad9b1615eae1545
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 01:05:06 +0900

    display links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -12,6 +12,24 @@ export const NavBar: React.FC = () => {
           >
             Home
           </Link>
+          <Link
+            to="/autobatch"
+            className="rounded px-3 py-2 text-gray-300 hover:bg-gray-700"
+          >
+            Auto-Batch
+          </Link>
+          <Link
+            to="/suspense"
+            className="rounded px-3 py-2 text-gray-300 hover:bg-gray-700"
+          >
+            Suspense
+          </Link>
+          <Link
+            to="/nested_suspense"
+            className="rounded px-3 py-2 text-gray-300 hover:bg-gray-700"
+          >
+            Nested-Suspense
+          </Link>
         </div>
       </div>
     </nav>
