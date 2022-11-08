6fed209bb34301a70f63ea432ed12df23916a515
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 01:55:41 +0900

    modify classname

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="flex h-screen flex-col items-center justify-center font-mono text-gray-600">
       <NavBar />
-      <div className="flex-w-screen flex-1 flex-col items-center justify-center">
+      <div className="flex w-screen flex-1 flex-col items-center justify-center">
         {children}
       </div>
     </div>
