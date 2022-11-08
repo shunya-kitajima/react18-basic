25020ca30a71a6b487681e454d48011e878b5806
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 01:10:24 +0900

    display NavBar and children and create type Props

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+import { NavBar } from './NavBar'
+
+type Props = {
+  children: ReactNode
+}
+
+export const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center font-mono text-gray-600">
+      <NavBar />
+      <div className="flex-w-screen flex-1 flex-col items-center justify-center">
+        {children}
+      </div>
+    </div>
+  )
+}
