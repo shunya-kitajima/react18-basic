76059bd9af8e57be3a8db8e8dceb0544ca1b5aee
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 15:08:38 +0900

    create query

---
+++ hooks/useQueryUsers.ts
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { delay } from '../utils/fetchDelay'
+
+export const useQueryUsers = () => {
+  const getUsers = async (): Promise<any> => {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/users?_limit=3'
+    )
+    await delay(3000)
+    return data
+  }
+
+  return useQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+  })
+}
