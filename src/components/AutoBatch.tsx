cfe3ee4b741a47b49c63b1eb59f9d45510794d09
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 18:16:00 +0900

    modify

--- src/components/AutoBatch.tsx
+++ src/components/AutoBatch.tsx
@@ -9,17 +9,17 @@ export const AutoBatch: React.FC = () => {
   const [users, setUsers] = useState([])
 
   const clickHandler = () => {
-    // setCount((count) => count + 1)
-    // setFetchCount((fetchCount) => fetchCount + 1)
+    setCount((count) => count + 1)
+    setFetchCount((fetchCount) => fetchCount + 1)
     return axios
       .get('https://jsonplaceholder.typicode.com/users')
       .then((res) => {
-        // flushSync(() => {
-        //   setUsers(res.data)
-        // })
-        // flushSync(() => {
-        //   setFetchCount((fetchCount) => fetchCount + 1)
-        // })
+        flushSync(() => {
+          setUsers(res.data)
+        })
+        flushSync(() => {
+          setFetchCount((fetchCount) => fetchCount + 1)
+        })
         setUsers(res.data)
         setFetchCount((fetchCount) => fetchCount + 1)
       })
@@ -32,6 +32,7 @@ export const AutoBatch: React.FC = () => {
       <p className="my-3 text-xl font-bold text-blue-500">AutoBatch</p>
       <p className="my-5">{count}</p>
       <p className="my-5">{fetchCount}</p>
+      <p className="my-5">{users}</p>
       <button
         className="my-5 rounded bg-indigo-600 px-3 py-1 text-white hover:bg-indigo-500"
         onClick={clickHandler}
