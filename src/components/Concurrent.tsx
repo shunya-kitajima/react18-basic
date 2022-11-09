692052412da886a99f5206f73d1ff87a192c9452
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 17:58:55 +0900

    import modules

---
+++ src/components/Concurrent.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { NavBar } from './NavBar'
+
+export const Concurrent: React.FC = () => {
+  return <div>Concurrent</div>
+}
