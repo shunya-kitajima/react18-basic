cce195b7a121da59392defc80743b45bc4f1ea55
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 11:11:09 +0900

    set states

--- src/components/AutoBatch.tsx
+++ src/components/AutoBatch.tsx
@@ -4,11 +4,16 @@ import { flushSync } from 'react-dom'
 import { Layout } from './Layout'
 
 export const AutoBatch: React.FC = () => {
+  const [count, setCount] = useState(0)
+  const [fetchCount, setFetchCount] = useState(0)
+  const [users, setUsers] = useState([])
+
   console.log('Rendered AutoBatch')
 
   return (
     <Layout>
-      <p>AutoBatch</p>
+      <p className="my-3 text-xl font-bold text-blue-500">AutoBatch</p>
+      <p className="my-5">{fetchCount}</p>
     </Layout>
   )
 }
