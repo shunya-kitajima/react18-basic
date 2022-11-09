3b53f0d094359365297f8922da36b3392815c836
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 15:17:43 +0900

    create query

---
+++ hooks/useQueryComments.ts
@@ -0,0 +1,18 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { delay } from '../utils/fetchDelay'
+
+export const useQueryComments = () => {
+  const getComments = async (): Promise<any> => {
+    const { data } = await axios.get(
+      'https://jsonplaceholder.typicode.com/comments?_limit=3'
+    )
+    await delay(1000)
+    return data
+  }
+
+  return useQuery({
+    queryKey: ['comments'],
+    queryFn: getComments,
+  })
+}
