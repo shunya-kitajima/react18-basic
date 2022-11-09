a05461010f1990368c9cdd9c8ed2645439e0dcbd
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 15:52:46 +0900

    display components

--- src/components/SuspenseDemo.tsx
+++ src/components/SuspenseDemo.tsx
@@ -8,5 +8,36 @@ import { FetchTasks } from './FetchTasks'
 import { FetchComments } from './FetchComments'
 
 export const SuspenseDemo: React.FC = () => {
-  return <div>SuspenseDemo</div>
+  return (
+    <Layout>
+      <p className="mb-3 text-xl font-bold text-blue-500">Suspense Demo</p>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchUsers />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchTasks />
+        </Suspense>
+      </ErrorBoundary>
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary>
+    </Layout>
+  )
 }
