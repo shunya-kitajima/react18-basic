a2ee8de4115ca9aa439d77b65f09950df98ee987
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 18:12:11 +0900

    display input

--- src/components/Concurrent.tsx
+++ src/components/Concurrent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, ChangeEvent } from 'react'
 import axios from 'axios'
 import { NavBar } from './NavBar'
 
@@ -18,5 +18,22 @@ export const Concurrent: React.FC = () => {
     return photo.title.includes(searchKey)
   })
 
-  return <div>Concurrent</div>
+  const updateHandler = (e: ChangeEvent<HTMLInputElement>): void => {
+    setSearchKey(e.target.value)
+  }
+
+  return (
+    <div className="flex flex-col items-center font-mono text-gray-600">
+      <NavBar />
+      <p className="my-3 text-xl font-bold text-blue-500">
+        startTransition (concurrent feature)
+      </p>
+      <input
+        type="text"
+        className="mb-5 rounded border border-gray-300 px-3 py-1 text-xs"
+        value={searchKey}
+        onChange={updateHandler}
+      />
+    </div>
+  )
 }
