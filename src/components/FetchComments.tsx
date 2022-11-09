cf8b1f8a363f23f4e6cc178a01f2e8be55726fed
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2022-11-09 15:41:31 +0900

    display comment name

---
+++ src/components/FetchComments.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useQueryComments } from '../hooks/useQueryComments'
+
+export const FetchComments: React.FC = () => {
+  const { data } = useQueryComments()
+
+  return (
+    <div className="my-3 text-center">
+      <p className="my-3 font-bold">Task List</p>
+      {data?.map((comment: any) => (
+        <p className="my-3 text-sm" key={comment.id}>
+          {comment.name}
+        </p>
+      ))}
+    </div>
+  )
+}
